48902ae568e6d01030862659692b9e300fc894d4
angie <angiemoralessilverio2@gmail.com>
2024-08-23 19:23:10 -0500

    version1

---
+++ src/main/java/pe/edu/upc/donfy/DonfyApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.donfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonfyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DonfyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/upc/donfy/DonfyApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.donfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonfyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
