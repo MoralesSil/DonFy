31298d73ba395a168499c71c63fd6a54f1a8ded8
John <johnluna2911@gmai.com>
2024-08-31 17:51:13 -0500

    Cambio

--- src/main/java/pe/edu/upc/donfy/dtos/NotificationsDTO.java
+++ src/main/java/pe/edu/upc/donfy/dtos/NotificationsDTO.java
@@ -1,9 +1,12 @@
 package pe.edu.upc.donfy.dtos;
 
+import pe.edu.upc.donfy.entities.NotificationType;
+
 public class NotificationsDTO {
     private int idNotificacion;
     private String mensaje;
     private String estado;
+    private NotificationType tipoNotificacion;
 
     public int getIdNotificacion() {
         return idNotificacion;
@@ -27,5 +30,12 @@ public class NotificationsDTO {
 
     public void setEstado(String estado) {
         this.estado = estado;
+    }
+    public NotificationType getTipoNotificacion() {
+        return tipoNotificacion;
+    }
+
+    public void setTipoNotificacion(NotificationType tipoNotificacion) {
+        this.tipoNotificacion = tipoNotificacion;
     }
 }
