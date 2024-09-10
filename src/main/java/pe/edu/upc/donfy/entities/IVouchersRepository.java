10d40627ba88fe3fbb4f681cea135e8e9e511efa
Camila Fernanda Morales Farias <118644922+CamMorales-v25@users.noreply.github.com>
2024-09-09 22:48:11 -0500

    borrado

--- src/main/java/pe/edu/upc/donfy/entities/IVouchersRepository.java
+++
@@ -1,6 +0,0 @@
-package pe.edu.upc.donfy.entities;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IVouchersRepository extends JpaRepository<Vouchers, Integer> {
-}
--- src/main/java/pe/edu/upc/donfy/repositories/IVouchersRepository.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upc.donfy.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upc.donfy.entities.Vouchers;
-
-public interface IVouchersRepository extends JpaRepository<Vouchers, Integer> {
-}
