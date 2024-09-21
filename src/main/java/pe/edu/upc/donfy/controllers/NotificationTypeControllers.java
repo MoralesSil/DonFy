dc4b64f077b78c234cd6b3b2c5c3d3845935a2f9
Golreddd <rquispegiraldo@gmail,com>
2024-09-20 20:05:19 -0500

    Gestionar tipo de notificacion como administrador

--- src/main/java/pe/edu/upc/donfy/controllers/NotificationTypeControllers.java
+++ src/main/java/pe/edu/upc/donfy/controllers/NotificationTypeControllers.java
@@ -2,6 +2,7 @@ package pe.edu.upc.donfy.controllers;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import pe.edu.upc.donfy.dtos.NotificationTypeDTO;
 import pe.edu.upc.donfy.entities.NotificationType;
@@ -17,6 +18,7 @@ public class NotificationTypeControllers {
     private INotificationTypeService ntS;
 
     @GetMapping
+    @PreAuthorize("hasAuthority('ADMINISTRADOR')")
     public List<NotificationTypeDTO> listar() {
 
         return ntS.list().stream().map(x -> {
@@ -25,24 +27,28 @@ public class NotificationTypeControllers {
         }).collect(Collectors.toList());
     }
     @PostMapping
+    @PreAuthorize("hasAuthority('ADMINISTRADOR')")
     public void registar(@RequestBody NotificationTypeDTO dto) {
         ModelMapper m = new ModelMapper();
         NotificationType nt = m.map(dto, NotificationType.class);
         ntS.insert(nt);
     }
     @GetMapping("/{idTipoNotificacion}")
+    @PreAuthorize("hasAuthority('ADMINISTRADOR')")
     public NotificationTypeDTO listarId(@PathVariable("idTipoNotificacion") Integer idTipoNotificacion  ) {
         ModelMapper m=new ModelMapper();
         NotificationTypeDTO dto=m.map(ntS.listId(idTipoNotificacion), NotificationTypeDTO.class);
         return dto;
     }
     @PutMapping
+    @PreAuthorize("hasAuthority('ADMINISTRADOR')")
     public void modificar(@RequestBody NotificationTypeDTO dto) {
         ModelMapper m=new ModelMapper();
         NotificationType nt=m.map(dto, NotificationType.class);
         ntS.update(nt);
     }
     @DeleteMapping("/{idTipoNotificacion}")
+    @PreAuthorize("hasAuthority('ADMINISTRADOR')")
     public void eliminar(@PathVariable("idTipoNotificacion") Integer idTipoNotificacion) {
         ntS.delete(idTipoNotificacion);
     }
